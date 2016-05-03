87196aaf9c239804494b35d015cf33de8d6b8d73
Github_LONGBO <550396038@qq.com>
2016-05-03 20:58:14 +0800

    数组第一课

---
+++ dasNeiLianxi3/Shuzu.java
@@ -0,0 +1,33 @@
+/**
+ * Created by asus on 2016/5/3.
+ */
+public class Shuzu {
+
+    public static void main(String[] args) {
+
+
+        int [] a = new int[4];
+        a[0]=11;
+        a[1]=22;
+        a[2]=33;
+        a[3]=44;
+        for (int i = 0;i<a.length;i++) {
+
+            System.out.println(a[i]);
+        }
+        System.out.println(" ");
+        int[] b  = new int[]{1,3,4,5,67,8,345};
+
+        for (int n = 0 ; n<b.length;n++){
+
+            System.out.println(b[n]);
+
+            }
+
+
+
+
+
+
+    }
+}
